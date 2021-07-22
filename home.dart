faba81d67f917f59f381b85c253858f5185f8a10
Hunain Mustafa <87203555+hunain77@users.noreply.github.com>
2021-07-22 04:49:44 -0700

    Add files via upload
    first commit

---
+++ home.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class Todo extends StatefulWidget {
+  @override
+  _TodoState createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+  var Output = "";
+  List<dynamic> lst = [1, 2, 3];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: lst.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 50,
+              color: Colors.amber,
+              margin: EdgeInsets.only(top: 15),
+              child: ListTile(
+                title: Text("${lst[index]}"),
+                trailing: Container(
+                  width: 50,
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text("Edit Item "),
+                                    content: TextField(onChanged: (value) {
+                                      Output = value;
+                                    }),
+                                    actions: [
+                                      ElevatedButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            setState(() {
+                                              lst.replaceRange(
+                                                  index, index + 1, {Output});
+                                            });
+                                          },
+                                          child: Text("Edit"))
+                                    ],
+                                  );
+                                });
+                          },
+                          child: Icon(Icons.edit)),
+                      GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              lst.removeAt(index);
+                            });
+                          },
+                          child: Icon(Icons.delete)),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Add Item "),
+                  content: TextField(onChanged: (value) {
+                    Output = value;
+                  }),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          setState(() {
+                            lst.add(Output);
+                          });
+                        },
+                        child: Text("Add"))
+                  ],
+                );
+              });
+        },
+        child: Text("Add "),
+      ),
+    );
+  }
+}
